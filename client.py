7f7d3ddab16dfe81af0437c4b0ea08cc6e569378
Victor Zhang <victor.g.zhang@gmail.com>
2018-08-01 19:37:40 +0900

    can now send various commands to the command line, and receive the output; except 'python --version' doesn't work

--- client.py
+++ client.py
@@ -6,16 +6,20 @@ local_hostname = socket.gethostname()
 
 localfqdn = socket.getfqdn()
 
-ip_address = "133.11.164.138" #socket.gethostbyname("")
+ip_address = "133.11.164.143" #socket.gethostbyname("")
 
 server_address = (ip_address, 7777)
 sock.connect(server_address)
 print("Connecting to %s (%s) at %s" % (local_hostname, localfqdn, ip_address))
 
-randomData = [243,45242,32,43,5,2,"SDF","123W","313"]
+randomData = ["ls", "pwd","git --version", "cd ../;pwd;ls"] # python --version doesn't seem to work!!  "homebrew --version"]
 for ele in randomData:
-    print("data is: ", str(ele))
-    new_data = str("rand1: %s\n" % ele).encode("utf-8")
-    sock.sendall(new_data)
+    print("command is: ", str(ele))
+    sock.sendall(ele.encode())
+    print("after sendall")
+    data = sock.recv(1024)
+    print("after recv")
+    dat = data.decode("utf-8")
+    print ("Data from server: %s" % dat)
 
 sock.close()
